123f461343c1e14b85bac6cab6bd0f4fb4b75d8d
suminb99 <suminbaek99@gmail.com>
2025-07-18 19:49:28 +0900

    #3 refactor: 전체 레이아웃 임시 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css';
+import Footer from './components/common/Footer';
 import Header from './components/common/Header';
 
 function App() {
-  return <Header />;
+  return (
+    <div className='min-h-screen overflow-x-hidden flex flex-col justify-between'>
+      <Header />
+      <main className='grow'> Content goes here</main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
