d7b75cd5c9938b2659b7df2f0033b4d45401512d
suminb99 <suminbaek99@gmail.com>
2025-07-24 15:24:44 +0900

    #6 feat: 기본 페이지 및 공통 컴포넌트 파일 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <div className='font-extrabold text-3xl text-red-400'>hello world!</div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
