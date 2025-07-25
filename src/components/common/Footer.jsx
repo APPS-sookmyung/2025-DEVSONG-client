141cf77fdd1f9c9ebe1b8b7d3ac279b2d21c3b1f
suminb99 <suminbaek99@gmail.com>
2025-07-25 18:25:58 +0900

    #3 style: footer에 반응형 디자인 적용

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,13 +2,15 @@ import logo from '../../assets/images/logo.svg';
 
 const Footer = () => {
   return (
-    <footer className='flex px-32 h-20 items-center justify-between text-xs bg-white'>
-      <img src={logo} alt='logo' />
-      <span>Devsong @ 2025. All rights reserved.</span>
-      <ul className='flex gap-10'>
-        <li>이용약관</li>
-        <li>개인정보 처리방침</li>
-      </ul>
+    <footer className='hidden md:flex'>
+      <div className='flex w-full bg-white justify-between md:px-6 items-center py-7 lg:px-32 '>
+        <img src={logo} alt='logo' />
+        <span>Devsong @ 2025. All rights reserved.</span>
+        <ul className='flex md:gap-4 lg:gap-12'>
+          <li>이용약관</li>
+          <li>개인정보 처리방침</li>
+        </ul>
+      </div>
     </footer>
   );
 };
