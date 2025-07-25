8e9d562435c344747b76cbacb171044d1bc3fb36
suminb99 <suminbaek99@gmail.com>
2025-07-25 12:49:40 +0900

    #2 feat: RootLayout에 header 추가

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/common/Navbar';
 import {Outlet} from 'react-router-dom';
+import Header from '../components/common/Header';
 
 const RootLayout = () => {
   return (
     <div className='min-h-screen flex flex-col'>
+      <Header />
       <div className='flex-1 bg-[#F9FAFC]'>
         <Outlet />
       </div>
