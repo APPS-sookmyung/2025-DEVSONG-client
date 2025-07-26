cd27f12966a59f3740bbb1f259be6a904102408b
suminb99 <suminbaek99@gmail.com>
2025-07-27 01:58:23 +0900

    #1 feat: RootLayout에 footer 추가

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Navbar from '../components/common/Navbar';
 import {Outlet} from 'react-router-dom';
 import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
 
 const RootLayout = () => {
   return (
     <div className='min-h-screen flex flex-col'>
       <Header />
-      <div className='flex-1 bg-[#F9FAFC]'>
+      <div className='flex-1'>
         <Outlet />
       </div>
+      <Footer />
       <Navbar />
     </div>
   );
