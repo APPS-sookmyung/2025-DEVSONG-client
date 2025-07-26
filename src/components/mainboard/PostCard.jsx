830531bb24e501d5e407fa29fa179f03e87713bb
suminb99 <suminbaek99@gmail.com>
2025-07-27 02:55:31 +0900

    #1 feat: 날짜 포맷 함수 구현 및 모집 카테고리에서만 모집현황 라벨 렌더링

--- src/components/mainboard/PostCard.jsx
+++ src/components/mainboard/PostCard.jsx
@@ -1,25 +1,65 @@
-const PostCard = () => {
+import RecruitLabel from '../common/RecruitLabel';
+import CategoryLabel from '../common/CategoryLabel';
+import comment from '../../assets/icons/comment.svg';
+import heart from '../../assets/icons/heart.svg';
+
+const formatDate = (createdAt) => {
+  const date = new Date(createdAt);
+  const currentDate = new Date();
+  const updated =
+    currentDate.toDateString() === date.toDateString()
+      ? date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+      : date
+          .toLocaleDateString('ko-KR', {
+            month: '2-digit',
+            day: '2-digit',
+          })
+          .replaceAll('.', '')
+          .replace(' ', '/');
+
+  return updated;
+};
+
+const PostCard = ({
+  title,
+  content,
+  author,
+  createdAt,
+  closed,
+  category,
+  countLike,
+  countComment,
+}) => {
+  const isRecruiting = ['프로젝트', '스터디', '대외활동'].includes(category)
+    ? true
+    : false;
   return (
-    <div className='flex flex-col justify-evenly w-212 h-55 border-0 rounded-xl px-5 bg-white'>
-      <span></span>
-      <span></span>
-      <div className='font-medium'>
-        <h3 className='text-xl font-bold'>프로젝트 팀원 모집 중</h3>
-        <p className='text-lg text-[#747577]'>
-          안녕하세요, iOS 앱 개발 프로젝트 팀원을 모집합니다. 안녕하세요, iOS 앱
-          개발 프로젝트 팀원을 모집합니다. 안녕하세요, iOS 앱 개발 프로젝트
-          팀원을 모집합니다. 안녕하세요, iOS 앱 개발 프로젝트 팀원을 모집합니다.
-          안녕하세요,...
-        </p>
-      </div>
-      <div className='flex justify-between text-base text-[#A3A3A4]'>
-        <div className='flex gap-2'>
-          <span>윤경빈</span>
-          <span>18:53</span>
+    <div className='flex flex-col bg-white max-w-86 min-h-32 px-4 py-3 rounded-xl gap-2 md:max-w-149 lg:max-w-212 lg:min-h-56'>
+      {/* 라벨 영역 */}
+      <div>
+        <div className='flex gap-1'>
+          <CategoryLabel category={category} />
+          {isRecruiting && (
+            <RecruitLabel status={closed ? '모집완료' : '모집중'} />
+          )}
         </div>
-        <div className='flex gap-2'>
-          <span>15</span>
-          <span>13</span>
+      </div>
+      <div className='text-sm font-bold'>{title}</div>
+      <p className='text-[10px] text-black-60'>{content}</p>
+      <div className='flex items-center justify-between'>
+        <div className='flex text-[8px] text-black-40 gap-1'>
+          <span>{author}</span>
+          <span>{formatDate(createdAt)}</span>
+        </div>
+        <div className='flex text-[10px] gap-[6px]'>
+          <span className='flex items-center'>
+            <img src={heart} alt='좋아요' />
+            {countLike}
+          </span>
+          <span className='flex items-center'>
+            <img src={comment} alt='좋아요' />
+            {countComment}
+          </span>
         </div>
       </div>
     </div>
