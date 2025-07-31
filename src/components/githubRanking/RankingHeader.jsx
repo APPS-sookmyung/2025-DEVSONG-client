d1e9d106f2c3e976921dfec64aaa5f6cc24d0965
Bangjh05 <everangel05@naver.com>
2025-07-31 13:02:00 +0900

    #4 feat: RankingTable 반응형 구현

--- src/components/githubRanking/RankingHeader.jsx
+++ src/components/githubRanking/RankingHeader.jsx
@@ -3,9 +3,13 @@ import back from '../../assets/icons/back.svg';
 
 export default function RankingHeader() {
   return (
-    <div className='flex gap-4 mb-5'>
-      <img className='self-start rounded-[4px]' src={back} alt='back' />
-      <span className='font-bold'>Github 랭킹</span>
+    <div className='flex items-center gap-4 mb-5 px-2 sm:px-0'>
+      <img
+        className='w-6 h-6 sm:w-7 sm:h-7 rounded-[4px] cursor-pointer'
+        src={back}
+        alt='back'
+      />
+      <span className='text-base sm:text-xl font-bold'>Github 랭킹</span>
     </div>
   );
 }
--- src/components/githubRanking/RankingTable.jsx
+++ src/components/githubRanking/RankingTable.jsx
@@ -2,26 +2,35 @@ import React from 'react';
 
 export default function RankingTable({pagedData, startIndex}) {
   return (
-    <div className='flex justify-center bg-white rounded-[12px] h-[550px] w-[850px] shadow-1g'>
+    <div
+      className='flex justify-center bg-white rounded-xl shadow-1g 
+            w-87 h-140
+            sm:w-140 sm:h-133 
+            md:w-175 md:h-133 
+            lg:w-210 lg:h-135'>
       <ul>
         <li className='flex justify-between px-1 py-2 font-semibold text-gray-500 mt-3'>
-          <span className='w-[45px] text-sm' style={{color: '#A3A3A4'}}>
+          <span className='w-11 text-sm' style={{color: '#A3A3A4'}}>
             순위
           </span>
           <span className='flex-1 text-sm' style={{color: '#A3A3A4'}}>
             이름
           </span>
           <span
-            className='w-[90px] text-right text-sm mr-3'
+            className='w-22 text-right text-sm mr-1 sm:mr-3'
             style={{color: '#A3A3A4'}}>
             Total
           </span>
         </li>
         {pagedData.map((user, index) => (
-          <li key={startIndex + index} className='flex justify-between p-3'>
-            <span className='font-bold mr-165'>
+          <li
+            key={startIndex + index}
+            className='flex justify-between p-1 sm:p-3'>
+            <span className='font-bold mr-50 sm:mr-100 md:mr-130 lg:mr-165'>
               <span className='mr-5'>
-                {String(startIndex + index + 1).padStart(2, ' ')}
+                {startIndex + index + 1 < 10
+                  ? '\u00A0\u00A0' + (startIndex + index + 1)
+                  : startIndex + index + 1}
               </span>
               <span>{user.name}</span>
             </span>
