a64d19378c6803906975386dfcd763f5eb3e2e99
suminb99 <suminbaek99@gmail.com>
2025-08-01 01:33:53 +0900

    #1 feat: RecruitmentStatusFilter 컴포넌트 구현

---
+++ src/components/mainboard/RecruitmentStatusFilter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ellipse from '../../assets/icons/ellipseIcon.svg';
+
+const status = [
+  {id: 'ALL', title: '전체'},
+  {id: 'OPEN', title: '모집중'},
+  {id: 'CLOSED', title: '모집완료'},
+];
+const RecruitmentStatusFilter = () => {
+  return (
+    <div>
+      <ul className='flex text-sm gap-5 text-black-80'>
+        {status.map((item) => {
+          return (
+            <div key={item.id} className='flex-center gap-1.5'>
+              <img src={ellipse} alt='' />
+              <li>{item.title}</li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default RecruitmentStatusFilter;
