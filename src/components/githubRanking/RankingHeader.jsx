fa5c7ccc6ac241e78e47831753a5ee9b233df471
Bangjh05 <everangel05@naver.com>
2025-07-31 15:54:23 +0900

    #4 remove: 쓸모없는 코드 삭제

--- src/components/githubRanking/RankingHeader.jsx
+++ src/components/githubRanking/RankingHeader.jsx
@@ -1,39 +1,5 @@
-// import React from 'react';
-// import back from '../../assets/icons/back.svg';
-
-// export default function RankingHeader() {
-//   return (
-//     <div className='flex gap-4 mb-5'>
-//       <img className='self-start rounded-[4px]' src={back} alt='back' />
-//       <span className='font-bold'>Github 랭킹</span>
-//     </div>
-//   );
-// }
-
 import React from 'react';
 import back from '../../assets/icons/back.svg';
-
-// export default function RankingHeader() {
-//   return (
-//     <div className='flex gap-4 p-3 mb-3 mr100 sm:p-0 sm:mb-5 bg-white sm:bg-transparent'>
-//       <img className='self-start rounded-xl' src={back} alt='back' />
-//       <span className='font-bold'>Github 랭킹</span>
-//     </div>
-//   );
-// }
-
-// export default function RankingHeader() {
-//   return (
-//     <div className='w-full sm:w-auto flex items-center gap-4 p-3 mb-3 sm:p-0 sm:mb-5 bg-white sm:bg-transparent'>
-//       <img
-//         className='self-start rounded-xl pl-3 sm:pl-0'
-//         src={back}
-//         alt='back'
-//       />
-//       <span className='font-bold items-center'>Github 랭킹</span>
-//     </div>
-//   );
-// }
 
 export default function RankingHeader() {
   return (
