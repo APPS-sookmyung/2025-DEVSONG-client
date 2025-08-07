908b46980d16b3971ca215ebb91168da2a98fc46
suminb99 <suminbaek99@gmail.com>
2025-08-07 20:18:33 +0900

    #1 style: 배경에 그라데이션 색상 추가

--- src/pages/MainBoard.jsx
+++ src/pages/MainBoard.jsx
@@ -8,6 +8,7 @@ import {useState} from 'react';
 const MainBoard = () => {
   return (
     <div className='relative flex flex-col items-center gap-2'>
+      <div className='absolute z-[-1] w-full h-62 md:h-91 lg:h-121 bg-linear-[154deg,#a4b8ff_0%,#a8d4ff_56.76%,#d9f6ff_86.78%] rounded-b-4xl'></div>
       <div>
         <SearchBar />
         <CategorySelector />
