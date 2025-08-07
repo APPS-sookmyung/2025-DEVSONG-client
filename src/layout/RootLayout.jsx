c8b47e0408978a17264a5beb9ba1ceea1b7bbc9e
suminb99 <suminbaek99@gmail.com>
2025-08-07 21:33:17 +0900

    #1 style: outlet 패딩값 수정

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -3,13 +3,12 @@ import Navbar from '../components/common/Navbar';
 import {Outlet} from 'react-router-dom';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
-import Footer from '../components/common/Footer';
 
 const RootLayout = () => {
   return (
     <div className='min-h-screen flex flex-col'>
       <Header />
-      <div className='flex-1 pb-30'>
+      <div className='flex-1 pb-30 md:pb-12'>
         <Outlet />
       </div>
       <Footer />
