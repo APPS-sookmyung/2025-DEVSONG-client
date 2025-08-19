0d27700077f69c41e86edef210c600f0eca64165
suminb99 <suminbaek99@gmail.com>
2025-08-19 13:44:36 +0900

    #16 feat: 댓글 액션 모달창 구현

--- src/components/post/CommentActions.jsx
+++ src/components/post/CommentActions.jsx
@@ -1,12 +1,27 @@
+import {useState} from 'react';
 import commentIcon from '../../assets/icons/comment.svg';
 import menuIcon from '../../assets/icons/verticalEllipsisIcon.svg';
+import ModalLayout from '../common/ModalLayout';
+import CommentActionsMenu from './CommentActionsMenu';
 
 const CommentActions = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className='ml-auto flex items-center bg-grey-02 w-16 h-6 px-[6px] py-[2px] rounded-sm md:w-24 md:h-8 md:px-3 md:py-1'>
+    <div className='cursor-pointer relative ml-auto flex items-center bg-grey-02 w-16 h-6 px-[6px] py-[2px] rounded-sm md:w-24 md:h-8 md:px-3 md:py-1'>
       <img src={commentIcon} className='w-5 h-5 md:w-6 md:h-6' alt='좋아요' />
       <div className='h-3 border-[0.8px] mx-[6px] md:mx-3'></div>
-      <img src={menuIcon} className='w-5 h-5 md:w-6 md:h-6' alt='메뉴' />
+      <img
+        src={menuIcon}
+        onClick={handleOnClick}
+        className='w-5 h-5 md:w-6 md:h-6'
+        alt='메뉴'
+      />
+      {isOpen && <CommentActionsMenu />}
     </div>
   );
 };
---
+++ src/components/post/CommentActionsMenu.jsx
@@ -0,0 +1,17 @@
+import ModalLayout from '../common/ModalLayout';
+
+const menuItems = ['수정', '삭제', '신고'];
+
+const CommentActionsMenu = () => {
+  return (
+    <ModalLayout width={'w-16 md:w-24'}>
+      {menuItems.map((item) => (
+        <div className='text-sm md:text-base font-semibold leading-[26.5px] text-black-100'>
+          {item}
+        </div>
+      ))}
+    </ModalLayout>
+  );
+};
+
+export default CommentActionsMenu;
