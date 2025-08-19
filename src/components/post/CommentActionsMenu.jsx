8df5426e40ad56ba4528ba534f9cd0b7cbd5d990
suminb99 <suminbaek99@gmail.com>
2025-08-19 14:05:51 +0900

    #16 feat: child element key값 설정

--- src/components/post/CommentActionsMenu.jsx
+++ src/components/post/CommentActionsMenu.jsx
@@ -5,8 +5,10 @@ const menuItems = ['수정', '삭제', '신고'];
 const CommentActionsMenu = () => {
   return (
     <ModalLayout width={'w-16 md:w-24'}>
-      {menuItems.map((item) => (
-        <div className='text-sm md:text-base font-semibold leading-[26.5px] text-black-100'>
+      {menuItems.map((item, index) => (
+        <div
+          key={index}
+          className='text-sm md:text-base font-semibold leading-[26.5px] text-black-100'>
           {item}
         </div>
       ))}
