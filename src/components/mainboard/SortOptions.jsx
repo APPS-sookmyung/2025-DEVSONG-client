e43602f576ec11b237d924505f3da23300b648bf
suminb99 <suminbaek99@gmail.com>
2025-08-20 21:11:50 +0900

    #16 refactor: SortSelector의 옵션 부분 SortOptions 컴포넌트로 분리

---
+++ src/components/mainboard/SortOptions.jsx
@@ -0,0 +1,18 @@
+import ModalLayout from '../common/ModalLayout';
+
+const optionItems = ['최신순', '관심순'];
+const SortOptions = () => {
+  return (
+    <ModalLayout width={'w-17 md:w-20 lg:w-24'}>
+      {optionItems.map((option, index) => (
+        <div
+          key={index}
+          className='text-xs md:text-sm lg:text-base font-medium text-black-100'>
+          {option}
+        </div>
+      ))}
+    </ModalLayout>
+  );
+};
+
+export default SortOptions;
--- src/components/mainboard/SortSelector.jsx
+++ src/components/mainboard/SortSelector.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import arrowdown from '../../assets/icons/arrowdownIcon.svg';
+import SortOptions from './SortOptions';
 
 const SortSelector = () => {
   const [sortType, setSortType] = useState('최신순');
@@ -17,12 +18,7 @@ const SortSelector = () => {
         {sortType}
         <img src={arrowdown} alt='자세히 보기' />
       </div>
-      {isOpen && (
-        <ul className='absolute w-17 text-xs md:w-20 lg:w-24 px-1.5 lg:px-2 bg-white rounded-md mt-1 shadow-box'>
-          <li className='p-1.5 border-b-1 border-black-20'>최신순</li>
-          <li className='p-1.5'>관심순</li>
-        </ul>
-      )}
+      {isOpen && <SortOptions />}
     </div>
   );
 };
