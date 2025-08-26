dba6ff8562b0dff75006a2a2a2abb8b428f33895
suminb99 <suminbaek99@gmail.com>
2025-08-26 09:22:06 +0900

    #16 feat: icon wrapper 및 뒤로가기 로직 추가

--- src/components/post/PostHeaderActions.jsx
+++ src/components/post/PostHeaderActions.jsx
@@ -1,16 +1,27 @@
 import verticalEllipsis from '../../assets/icons/verticalEllipsisIcon.svg';
-import back from '../../assets/icons/back.svg';
+import back from '../../assets/icons/back_grey.svg';
+import {useNavigate} from 'react-router-dom';
 
 const PostHeaderActions = ({isAuthor}) => {
+  const navigate = useNavigate();
+
   return (
     <div
-      className={`hidden md:pb-5 md:flex md:justify-between *:md:bg-grey-01 *:rounded-sm *:p-[2px] *:w-6 *:h-6`}>
-      <img src={back} alt='뒤로가기' />
-      <img
-        className={`${!isAuthor ? 'md:hidden' : ''}`}
-        src={verticalEllipsis}
-        alt='메뉴'
-      />
+      className={`hidden md:pb-5 md:flex md:justify-between *:md:bg-grey-01 *:rounded-sm *:cursor-pointet`}>
+      <div
+        onClick={() => navigate(-1)}
+        className='flex-center w-7 h-7 px-[5.5px] py-[10.5px] cursor-pointer'>
+        <img src={back} alt='뒤로가기' />
+      </div>
+      <div>
+        <img
+          className={`w-7 h-7 p-0.5 cursor-pointer ${
+            !isAuthor ? 'md:hidden' : ''
+          }`}
+          src={verticalEllipsis}
+          alt='메뉴'
+        />
+      </div>
     </div>
   );
 };
