22c72a0fead14229e00df658af3288c9e5c9f14f
suminb99 <suminbaek99@gmail.com>
2025-08-26 09:40:16 +0900

    #16 style: 음영 적용

--- src/components/post/CommentBar.jsx
+++ src/components/post/CommentBar.jsx
@@ -2,7 +2,7 @@ import submit from '../../assets/icons/submitIcon.svg';
 
 const CommentBar = () => {
   return (
-    <form className='bg-white w-full fixed bottom-0 md:static md:bottom-auto px-6 pt-[10px] pb-[26px] md:rounded-b-3xl'>
+    <form className='bg-white w-full fixed bottom-0 md:static md:bottom-auto px-6 pt-[10px] pb-[26px] md:rounded-b-3xl shadow-box'>
       <div className='bg-grey-01 w-full flex gap-1 py-3 pl-[14px] rounded-lg'>
         <input
           className='flex-1 text-sm leading-[22.4px] focus:outline-none'
