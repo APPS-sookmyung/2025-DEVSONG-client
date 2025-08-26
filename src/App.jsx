5b06b5b6c808f6c4a0af5d51e635a0254588c3ec
suminb99 <suminbaek99@gmail.com>
2025-08-26 12:31:06 +0900

    #16 remove: 불필요한 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-  Navigate,
 } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
 import MainBoard from './pages/MainBoard';
