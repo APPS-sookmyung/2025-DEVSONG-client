45066dbdbf30e6b476edb5498681e90b9bbfea44
suminb99 <suminbaek99@gmail.com>
2025-08-26 09:39:20 +0900

    #16 refactor: 댓글 액션 메뉴의 옵션으로 삭제만 남기기

--- src/components/post/CommentActionsMenu.jsx
+++ src/components/post/CommentActionsMenu.jsx
@@ -1,17 +1,11 @@
 import ModalLayout from '../common/ModalLayout';
 
-const menuItems = ['수정', '삭제', '신고'];
-
 const CommentActionsMenu = () => {
   return (
     <ModalLayout width={'w-16 md:w-24'}>
-      {menuItems.map((item, index) => (
-        <div
-          key={index}
-          className='text-sm md:text-base font-semibold leading-[26.5px] text-black-100'>
-          {item}
-        </div>
-      ))}
+      <div className='text-sm md:text-base font-semibold leading-[26.5px] text-black-100'>
+        {'삭제'}
+      </div>
     </ModalLayout>
   );
 };
