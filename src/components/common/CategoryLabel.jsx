18d103bc069aaa4c5923d8faaaa2eed2d62bc8ae
suminb99 <suminbaek99@gmail.com>
2025-08-28 17:22:59 +0900

    #20 refactor: Category 배열을 key/label 구조의 CATEGORIES로 변경

--- src/components/common/CategoryLabel.jsx
+++ src/components/common/CategoryLabel.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
-const categoryStyle = {
-  프로젝트: 'bg-[#E5FFDF] text-[#7DBD6E]',
-  스터디: 'bg-[#FBFFDF] text-[#999D80]',
-  대외활동: 'bg-[#FFE6DF] text-[#9D8882]',
-  정보: 'bg-[#FFF2DF] text-[#9A8E7D]',
-  자유: 'bg-[#F1F0EF] text-[#A09E9C]',
+const category_map = {
+  project: {style: 'bg-[#E5FFDF] text-[#7DBD6E]', label: '프로젝트'},
+  study: {style: 'bg-[#FBFFDF] text-[#999D80]', label: '스터디'},
+  extra: {style: 'bg-[#FFE6DF] text-[#9D8882]', label: '대외 활동'},
+  info: {style: 'bg-[#FFF2DF] text-[#9A8E7D]', label: '정보'},
+  free: {style: 'bg-[#F1F0EF] text-[#A09E9C]', label: '자유'},
 };
 const CategoryLabel = ({category}) => {
   return (
     <div>
-      <span className={`label-style ${categoryStyle[category]}`}>
-        {category}
-      </span>
+      {category && (
+        <span className={`label-style ${category_map[category].style}`}>
+          {category_map[category].label}
+        </span>
+      )}
     </div>
   );
 };
