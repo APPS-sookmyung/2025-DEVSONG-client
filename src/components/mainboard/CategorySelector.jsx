17318a6826de3198c87a20aae4406eb147ea891c
suminb99 <suminbaek99@gmail.com>
2025-08-28 17:23:22 +0900

    #20 refactor: 카테고리 매핑을 영문 키 기반으로 통일하고 label+style 포함하도록 변경

--- src/components/mainboard/CategorySelector.jsx
+++ src/components/mainboard/CategorySelector.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const Category = [
-  {id: 0, title: '전체'},
-  {id: 1, title: '프로젝트'},
-  {id: 2, title: '스터디'},
-  {id: 3, title: '정보'},
-  {id: 4, title: '대외활동'},
-  {id: 5, title: '자유'},
+const CATEGORIES = [
+  {key: 'all', label: '전체'},
+  {key: 'project', label: '프로젝트'},
+  {key: 'study', label: '스터디'},
+  {key: 'info', label: '정보'},
+  {key: 'extra', label: '대외활동'},
+  {key: 'free', label: '자유'},
 ];
 
 const CategorySelector = () => {
   return (
     <div>
       <ul className='mb-10 md:mb-15 lg:mb-20 flex justify-evenly items-center text-sm md:text-base lg:text-lg text-black-100 font-semibold'>
-        {Category.map((item) => {
+        {CATEGORIES.map((category) => {
           return (
             <li
               className='cursor-pointer lg:pb-[5px] lg:px-[2.5px] lg:border-b-2 lg:border-transparent lg:hover:text-main lg:hover:border-main'
-              key={item.id}>
-              {item.title}
+              key={category.key}>
+              <Link
+                to={
+                  category.key !== 'all' ? `/posts/${category.key}` : `/posts`
+                }>
+                {category.label}
+              </Link>
             </li>
           );
         })}
