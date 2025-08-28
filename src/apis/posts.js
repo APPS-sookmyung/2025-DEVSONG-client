3a8e095c8d10544d055dc65629fb9d6799578b4d
suminb99 <suminbaek99@gmail.com>
2025-08-28 17:10:15 +0900

    #20 refactor: 게시물 조회 시 카테고리 쿼리 파라미터 추가

--- src/apis/posts.js
+++ src/apis/posts.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 
 const SERVER_URL = 'http://localhost:4000';
 
+export const getPosts = async ({category}) => {
+  const response = await axios.get(`${SERVER_URL}/posts`, {params: {category}});
+  return response;
+};
+
 export const getPostDetail = async ({post_id}) => {
-  const response = await axios.get(SERVER_URL + `/post/${post_id}`);
+  const response = await axios.get(`${SERVER_URL}/post/${post_id}`);
   return response;
 };
