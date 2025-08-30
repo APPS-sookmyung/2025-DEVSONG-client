8e118a42432dd32ef9a19bce08254bb36ea42f62
suminb99 <suminbaek99@gmail.com>
2025-08-30 13:10:02 +0900

    #21 feat: 게시글 작성 페이지 컴포넌트 추가

---
+++ src/pages/PostForm.jsx
@@ -0,0 +1,13 @@
+import CategoryOptions from '../components/postForm/CategoryOptions';
+import PostEditor from '../components/postForm/postEditor';
+
+const PostForm = () => {
+  return (
+    <div className=''>
+      <CategoryOptions />
+      <PostEditor />
+    </div>
+  );
+};
+
+export default PostForm;
