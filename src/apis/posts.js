72acfc9502b86599c566b54e23544e0b97037784
suminb99 <suminbaek99@gmail.com>
2025-08-30 15:29:53 +0900

    #20 feat: 임시 로그인 api 구현 및 api 요청 시 jwt 헤더에 담아 전달

--- src/apis/posts.js
+++ src/apis/posts.js
@@ -1,13 +1,37 @@
 import axios from 'axios';
 
-const SERVER_URL = 'http://localhost:4000';
+const SERVER_URL = 'http://localhost:8080';
+
+export const login = async (userinfo) => {
+  const response = axios.post(`${SERVER_URL}/user/login`, {
+    email: userinfo.email,
+    password: userinfo.password,
+  });
+
+  const {accessToken} = response.token;
+  if (accessToken) {
+    localStorage.setItem('accessToken', accessToken);
+  }
+
+  return response;
+};
 
 export const getPosts = async ({category}) => {
-  const response = await axios.get(`${SERVER_URL}/posts`, {params: {category}});
+  const token = localStorage.getItem('accessToken');
+  const response = await axios.get(`${SERVER_URL}/posts`, {
+    params: {category},
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response;
 };
 
 export const getPostDetail = async ({post_id}) => {
-  const response = await axios.get(`${SERVER_URL}/post/${post_id}`);
+  const response = await axios.get(`${SERVER_URL}/post/${post_id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response;
 };
