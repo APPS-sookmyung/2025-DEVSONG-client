721d2e4187917bd98025c35d7b45ec3f23532e0b
suminb99 <suminbaek99@gmail.com>
2025-09-14 01:20:17 +0900

    #21 chore: CategoryOptions 컴포넌트의 스타일 변수 선언을 템플릿 리터럴로 변경

--- src/components/postForm/CategoryOptions.jsx
+++ src/components/postForm/CategoryOptions.jsx
@@ -1,10 +1,11 @@
+import {useState} from 'react';
 import {CATEGORIES} from '../constants/categories';
 
-const base =
-  'cursor-pointer bg-white whitespace-nowrap rounded-3xl shadow-box font-semibold text-black-60 hover:bg-main hover:text-white';
+const base = `cursor-pointer bg-white whitespace-nowrap 
+rounded-3xl shadow-box font-semibold text-black-60 hover:bg-main hover:text-white`;
 const mobile = 'w-15 text-xs leading-4 px-2 py-1';
-const responsive =
-  'md:w-20.5 md:text-base md:leading-5 md:px-3 md:py-2 lg:w-26 lg:text-lg lg:leading-6';
+const responsive = `md:w-20.5 md:text-base md:leading-5 md:px-3 md:py-2 
+lg:w-26 lg:text-lg lg:leading-6`;
 
 const CategoryOptions = () => {
   return (
