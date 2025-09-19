f3922c372c2bb7bfe719e46484bfd6d227b1841d
suminb99 <suminbaek99@gmail.com>
2025-09-19 23:30:04 +0900

    #28 refactor: 옵션 컴포넌트 분리 및 상태 관리/네이밍 개선

--- src/components/postForm/CategoryOptions.jsx
+++ src/components/postForm/CategoryOptions.jsx
@@ -1,19 +1,17 @@
-import {useState} from 'react';
 import {CATEGORIES} from '../constants/categories';
+import Option from './Option';
 
-const base = `cursor-pointer bg-white whitespace-nowrap 
-rounded-3xl shadow-box font-semibold text-black-60 hover:bg-main hover:text-white`;
-const mobile = 'w-15 text-xs leading-4 px-2 py-1';
-const responsive = `md:w-20.5 md:text-base md:leading-5 md:px-3 md:py-2 
-lg:w-26 lg:text-lg lg:leading-6`;
-
-const CategoryOptions = () => {
+const CategoryOptions = ({selectedCategory, onCategorySelect}) => {
   return (
     <ul className='flex text-center justify-center md:justify-start gap-3 md:gap-6'>
       {CATEGORIES.slice(1).map((category) => (
-        <li key={category.label} className={`${base} ${mobile} ${responsive}`}>
-          {category.label}
-        </li>
+        <Option
+          key={category.key}
+          label={category.label}
+          category={category.key}
+          selected={selectedCategory}
+          onCategorySelect={onCategorySelect}
+        />
       ))}
     </ul>
   );
