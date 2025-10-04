3e53aefddba4639a56f109b70ee1474ae7193e5e
suminb99 <suminbaek99@gmail.com>
2025-10-05 00:41:29 +0900

    #32 style: 정렬 모달창 스타일링 수정 (간격, 폰트, 패딩 등)

--- src/components/common/ModalLayout.jsx
+++ src/components/common/ModalLayout.jsx
@@ -1,7 +1,7 @@
 const ModalLayout = ({children, width}) => {
   return (
     <div
-      className={`bg-white absolute z-1 shadow-box ${width} px-2 rounded-lg right-0 top-10`}>
+      className={`bg-white absolute z-1 shadow-box ${width} px-2 rounded-lg right-0 top-9 md:top-10 lg:top-12.5`}>
       <div className='flex flex-col *:py-2 *:not-last:border-b-1 *:border-black-20 *:hover:text-main cursor-pointer'>
         {children}
       </div>
