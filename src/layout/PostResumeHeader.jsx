1c3b66b4024764505d622a15976bd8ae119a13d6
suminb99 <suminbaek99@gmail.com>
2025-11-15 18:59:30 +0900

    #36 feat: 이력서, 게시글 작성 페이지 전용 헤더 추가

---
+++ src/layout/PostResumeHeader.jsx
@@ -0,0 +1,26 @@
+import verticalEllipsis from '../assets/icons/vertical_ellipsis.svg';
+import back from '../assets/icons/back2.svg';
+import {useNavigate} from 'react-router-dom';
+
+const PostResumeHeader = ({onClick}) => {
+  const navigate = useNavigate();
+
+  return (
+    <header className='bg-white w-full h-16 px-6 flex items-center justify-between *:cursor-pointer md:hidden'>
+      <img
+        className='w-5 h-5'
+        onClick={() => navigate(-1)}
+        src={back}
+        alt='뒤로가기'
+      />
+      <img
+        onClick={() => onClick()}
+        className='w-6 h-6'
+        src={verticalEllipsis}
+        alt='메뉴'
+      />
+    </header>
+  );
+};
+
+export default PostResumeHeader;
