3148f54fe56b672003eeef89d4a422e111a1eb9e
suminb99 <suminbaek99@gmail.com>
2025-12-27 14:08:58 +0900

    #52 style: 모집현황 옵션 선택 시 추가 스타일 적용

--- src/components/mainboard/RecruitmentStatusFilter.jsx
+++ src/components/mainboard/RecruitmentStatusFilter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import ellipse from '../../assets/icons/ellipseIcon.svg';
 
 const status = [
@@ -7,12 +7,20 @@ const status = [
   {id: 'CLOSED', title: '모집완료'},
 ];
 const RecruitmentStatusFilter = () => {
+  const [isClicked, setIsClicked] = useState('ALL');
+  const clickedStyle = 'text-black-100 font-medium';
+
   return (
     <div>
-      <ul className='cursor-pointer flex text-sm gap-5 md:text-base lg:text-lg md:gap-9 text-black-80'>
+      <ul className='cursor-pointer flex text-xs gap-5 md:text-base lg:text-lg md:gap-9 text-black-80'>
         {status.map((item) => {
           return (
-            <div key={item.id} className='flex-center gap-1.5'>
+            <div
+              key={item.id}
+              className={`${
+                item.id === isClicked ? clickedStyle : ''
+              } flex-center gap-1.5 py-2`}
+              onClick={() => setIsClicked(item.id)}>
               <img src={ellipse} alt='' />
               <li>{item.title}</li>
             </div>
