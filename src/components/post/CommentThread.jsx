f4f618dd2829a8b7fd80911daa6a758b6f0ca04c
suminb99 <suminbaek99@gmail.com>
2025-12-28 14:55:37 +0900

    #62 style: 작성자 확인 후 조건부 스타일링 적용

--- src/components/post/CommentThread.jsx
+++ src/components/post/CommentThread.jsx
@@ -4,27 +4,32 @@ import commentIcon from '../../assets/icons/comment_grey.svg';
 import menuIcon from '../../assets/icons/verticalEllipsisIcon.svg';
 import {useState} from 'react';
 import ModalLayout from '@components/common/ModalLayout';
+import {formatDate} from '../../lib/formateDate';
 
-export function CommentHeader({username, createdAt, handleSetClicked}) {
+function CommentHeader({isAuthor, username, createdAt, handleSetClicked}) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOnClick = () => {
     setIsOpen(!isOpen);
   };
 
+  console.log(isAuthor);
+
   return (
     <>
       <div className='flex items-center gap-2 text-black-40 text-xs font-medium'>
-        <p>{username}</p>
-        <p>{createdAt}</p>
-        <div className='cursor-pointer relative ml-auto flex items-center bg-grey-02 w-16 h-6 px-[6px] py-[2px] rounded-sm md:w-24 md:h-8 md:px-3 md:py-1'>
+        <p className={`${isAuthor === true ? 'text-main' : 'text-black-40'}`}>
+          {username}
+        </p>
+        <p>{formatDate(createdAt)}</p>
+        <div className='cursor-pointer relative ml-auto flex items-center bg-grey-02 w-16 h-6 px-1.5 py-0.5 rounded-sm md:w-24 md:h-8 md:px-3 md:py-1'>
           <img
             src={commentIcon}
             className='w-5 h-5 md:w-6 md:h-6'
             alt='답글'
             onClick={handleSetClicked}
           />
-          <div className='h-3 border-[0.8px] mx-[6px] md:mx-3'></div>
+          <div className='h-3 border-[0.8px] mx-1.5 md:mx-3'></div>
           <img
             src={menuIcon}
             onClick={handleOnClick}
@@ -48,6 +53,7 @@ export function CommentHeader({username, createdAt, handleSetClicked}) {
 
 const CommentThread = ({
   commentId,
+  isAuthor,
   index,
   isActive,
   onToggle,
@@ -62,13 +68,14 @@ const CommentThread = ({
   };
 
   return (
-    <div className={`${index !== 0 ? 'border-t-1 border-black-20' : ''} pt-3`}>
+    <div className={`${index !== 0 ? 'border-t border-black-20' : ''} pt-3`}>
       <div
         onClick={handleClick}
         className={`${
-          isActive ? 'bg-main/6 rounded-[12px] mx-[-9px] px-[9px]' : ''
+          isActive ? 'bg-main/6 rounded-xl -mx-2.25 px-2.25' : ''
         } pt-4 pb-3 md:py-4 mb-1.5`}>
         <CommentHeader
+          isAuthor={isAuthor}
           username={username}
           createdAt={createdAt}
           handleSetClicked={handleClick}
