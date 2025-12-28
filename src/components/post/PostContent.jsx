2b37e0695e42a645c81dc6e1fd3f921fff76e503
suminb99 <suminbaek99@gmail.com>
2025-12-28 19:31:38 +0900

    #62 feat: 포스트에 ContentPreview 컴포넌트 적용

--- src/components/post/PostContent.jsx
+++ src/components/post/PostContent.jsx
@@ -1,8 +1,11 @@
+import ContentPreview from '@components/common/ContentPreview';
+
 const PostContent = ({content}) => {
   return (
-    <p className='whitespace-pre-wrap text-sm font-medium md:text-base'>
-      {content}
-    </p>
+    // <p className='whitespace-pre-wrap text-sm font-medium md:text-base'>
+    //   {content}
+    // </p>
+    <ContentPreview size='none'>{content}</ContentPreview>
   );
 };
 
