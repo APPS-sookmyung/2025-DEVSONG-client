dd738bff6902294234debff502e73e0672ede1da
suminb99 <suminbaek99@gmail.com>
2025-12-28 17:41:26 +0900

    #62 feat: 마감 / 수정 옵션 모달

--- src/components/post/PostHeaderActions.jsx
+++ src/components/post/PostHeaderActions.jsx
@@ -28,7 +28,7 @@ const PostHeaderActions = ({isAuthor, postId, handlePostUpdate}) => {
         className='flex-center w-7 h-7 px-[5.5px] py-[10.5px] cursor-pointer'>
         <img src={back} alt='뒤로가기' />
       </div>
-      <div>
+      <div className='relative'>
         <img
           className={`w-7 h-7 p-0.5 cursor-pointer ${
             !isAuthor ? 'md:hidden' : ''
@@ -39,21 +39,21 @@ const PostHeaderActions = ({isAuthor, postId, handlePostUpdate}) => {
           src={verticalEllipsis}
           alt='메뉴'
         />
-      </div>
-      <>
         {isOpen && (
-          <ModalLayout>
-            {optionItems.map((option, index) => (
-              <div
-                key={index}
-                onClick={option === '마감' ? handleClose : handlePostUpdate}
-                className='text-xs md:text-sm lg:text-base font-medium text-black-100 lg:pl-1.5'>
-                {option}
-              </div>
-            ))}
-          </ModalLayout>
+          <div className='absolute whitespace-nowrap z-10 -top-1 right-0'>
+            <ModalLayout width={'w-17 md:w-20 lg:w-24'}>
+              {optionItems.map((option, index) => (
+                <div
+                  key={index}
+                  onClick={option === '마감' ? handleClose : handlePostUpdate}
+                  className='text-xs md:text-sm lg:text-base font-medium text-black-100 lg:pl-1.5'>
+                  {option}
+                </div>
+              ))}
+            </ModalLayout>
+          </div>
         )}
-      </>
+      </div>
     </div>
   );
 };
