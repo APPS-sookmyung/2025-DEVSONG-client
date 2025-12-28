451e0ca169efb3d65e3003ec9011af3359baaaa5
suminb99 <suminbaek99@gmail.com>
2025-12-28 20:15:46 +0900

    #67 feat: 로그인에서 회원가입 페이지 이동 적용

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import logo from '../assets/images/logo.svg';
 import {login} from '../apis/login';
 
@@ -80,9 +80,9 @@ const LoginPage = () => {
           </div>
 
           <div className='flex justify-center mt-6 text-sm text-black-60'>
-            <a href='#' className='hover:text-black-60'>
+            <Link to='/signup' className='hover:text-black-60'>
               회원가입
-            </a>
+            </Link>
             <span className='mx-4'>|</span>
             <a href='#' className='hover:text-black-60'>
               비밀번호 초기화
