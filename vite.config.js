2d457b595ac39c9a39d759f1f19f1694bdeb633b
suminb99 <suminbaek99@gmail.com>
2026-02-20 08:53:53 +0900

    #76 chore: @hooks 경로 별칭 추가 및 커스텀 훅 구현

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
       '@lib': path.resolve(__dirname, './src/lib'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
     },
   },
   server: {
