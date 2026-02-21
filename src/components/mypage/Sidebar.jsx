66e3c7c0b4fd0882b221f78a8239d9df5e031a4d
suminb99 <suminbaek99@gmail.com>
2026-02-22 00:20:41 +0900

    #84 refactor: MENU_OPTIONS constant.js로 분리

--- src/components/mypage/Sidebar.jsx
+++ src/components/mypage/Sidebar.jsx
@@ -1,37 +1,5 @@
-import commentIcon from '@assets/icons/comment.svg';
-import likedIcon from '@assets/icons/heart.svg';
 import nextArrow from '@assets/icons/nextArrow.svg';
-import editIcon from '@assets/icons/editIcon.svg';
-import my from '@assets/icons/nav_my.svg';
-import apply from '@assets/icons/apply.svg';
-
-const MENU_OPTIONS = [
-  {
-    id: 'edit',
-    label: '프로필 수정',
-    icon: my,
-  },
-  {
-    id: 'written',
-    label: '내가 쓴 글',
-    icon: editIcon,
-  },
-  {
-    id: 'comments',
-    label: '댓글 단 글',
-    icon: commentIcon,
-  },
-  {
-    id: 'liked',
-    label: '좋아요한 글',
-    icon: likedIcon,
-  },
-  {
-    id: 'applied',
-    label: '지원한 글',
-    icon: apply,
-  },
-];
+import {MENU_OPTIONS} from './constant';
 
 const Sidebar = ({
   activeTab,
