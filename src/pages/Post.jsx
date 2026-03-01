d950a2e5a9b208b9a09fd48cf5f353373c8acfff
suminb99 <suminbaek99@gmail.com>
2026-03-01 19:36:32 +0900

    #89 fix: CommentBar ->  MessageInput 교체

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -3,11 +3,11 @@ import PostResumeHeader from '../layout/PostResumeHeader';
 import PostContent from '@components/post/PostContent';
 import PostActions from '@components/post/PostActions';
 import CommentSection from '@components/post/CommentSection';
-import CommentBar from '@components/post/CommentBar';
 import {useCallback, useEffect, useState} from 'react';
 import {createComment, getPostDetail} from '@apis/posts';
 import {useNavigate, useParams} from 'react-router-dom';
 import PostLayout from '@components/post/PostLayout';
+import MessageInput from '@components/common/MessageInput';
 
 const Post = () => {
   const {id} = useParams(); // post id 받아오기
@@ -161,8 +161,7 @@ const Post = () => {
                 />
               </section>
             </section>
-
-            <CommentBar onAddComment={onAddComment} />
+            <MessageInput onSubmitMessage={onAddComment} />
           </div>
         )}
       </PostLayout>
